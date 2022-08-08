38355d09a4c48e2b40df9150b1005110775b55cc
jackpf <jack_farrelly@yahoo.co.uk>
2022-08-08 12:43:45 +0200

    Asset format tests

--- src/main/util/currency_util_test.go
+++ src/main/util/currency_util_test.go
@@ -1,10 +1,23 @@
 package util
 
 import (
+	configmodel "github.com/jackpf/kraken-scheduler/src/main/config/model"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestFormatAsset_Fiat(t *testing.T) {
+	result := FormatAsset(configmodel.EUR, 123.456)
+
+	assert.Equal(t, "€123.46", result)
+}
+
+func TestFormatAsset_Crypto(t *testing.T) {
+	result := FormatAsset(configmodel.XBT, 123.4567891011)
+
+	assert.Equal(t, "123.4567891₿", result)
+}
 
 func TestFormatAmount(t *testing.T) {
 	result := FormatFloat(12.34567891011, 8)
