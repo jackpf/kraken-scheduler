0f6761813918745916169aa9e60e7e174ff1dc7c
Jack Farrelly <jack.farrelly@soundcloud.com>
2024-09-30 05:06:18 +0200

    Add asset & currency labels to purchase metrics

--- src/main/metrics/metrics.go
+++ src/main/metrics/metrics.go
@@ -6,44 +6,47 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+var assetLabels = []string{"asset", "asset_symbol"}
+var currencyLabels = []string{"currency", "currency_symbol"}
+
 func NewMetrics() Metrics {
 	return &MetricsImpl{
 		orderCounter: promauto.NewCounterVec(prometheus.CounterOpts{
 			Name: "kraken_scheduler_orders_total",
 			Help: "The total number of orders made",
-		}, []string{"asset", "asset_symbol", "currency", "currency_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		purchaseCounter: promauto.NewCounterVec(prometheus.CounterOpts{
 			Name: "kraken_scheduler_purchases_total",
 			Help: "The total number of purchases made",
-		}, []string{"asset", "asset_symbol", "currency", "currency_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		assetPurchaseAmountGauge: promauto.NewGaugeVec(prometheus.GaugeOpts{
-			Name: "kraken_scheduler_asset_purchase_amount",
+			Name: "kraken_scheduler_purchase_amount",
 			Help: "How much of an asset was purchased",
-		}, []string{"asset", "asset_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		assetPurchaseAmountHistogram: promauto.NewHistogramVec(prometheus.HistogramOpts{
-			Name: "kraken_scheduler_asset_purchase_amount_history",
+			Name: "kraken_scheduler_purchase_amount_history",
 			Help: "How much of an asset was purchased",
-		}, []string{"asset", "asset_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		currencySpendAmountGauge: promauto.NewGaugeVec(prometheus.GaugeOpts{
-			Name: "kraken_scheduler_currency_spend_amount",
+			Name: "kraken_scheduler_spend_amount",
 			Help: "How much currency was spent on a purchase",
-		}, []string{"currency", "currency_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		currencySpendAmountHistogram: promauto.NewHistogramVec(prometheus.HistogramOpts{
-			Name: "kraken_scheduler_currency_spend_amount_history",
+			Name: "kraken_scheduler_spend_amount_history",
 			Help: "How much currency was spent on a purchase",
-		}, []string{"currency", "currency_symbol"}),
+		}, append(currencyLabels, assetLabels...)),
 		assetBalanceAmountGauge: promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "kraken_scheduler_asset_balance_amount",
 			Help: "How much of an asset currently exists on the account",
-		}, []string{"asset", "asset_symbol"}),
+		}, assetLabels),
 		assetBalanceValueGauge: promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "kraken_scheduler_asset_balance_value",
 			Help: "How much value of an asset exists on the account",
-		}, []string{"asset", "asset_symbol"}),
+		}, assetLabels),
 		currencyBalanceAmountGauge: promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "kraken_scheduler_currency_balance_amount",
 			Help: "How much of a currency currently exists on the account",
-		}, []string{"currency", "currency_symbol"}),
+		}, currencyLabels),
 		errorsCounter: promauto.NewCounter(prometheus.CounterOpts{
 			Name: "kraken_scheduler_errors_total",
 			Help: "The total number of errors during purchase process",
@@ -83,10 +86,10 @@ func (m *MetricsImpl) LogOrder(pair model.Pair) {
 
 func (m *MetricsImpl) LogPurchase(pair model.Pair, amount float64, fiatAmount float64, holdings float64, holdingsValue float64) {
 	m.purchaseCounter.WithLabelValues(pair.First.Name, pair.First.Symbol, pair.Second.Name, pair.Second.Symbol).Inc()
-	m.assetPurchaseAmountGauge.WithLabelValues(pair.First.Name, pair.First.Symbol).Set(amount)
-	m.assetPurchaseAmountHistogram.WithLabelValues(pair.First.Name, pair.First.Symbol).Observe(amount)
-	m.currencySpendAmountGauge.WithLabelValues(pair.Second.Name, pair.Second.Symbol).Set(fiatAmount)
-	m.currencySpendAmountHistogram.WithLabelValues(pair.Second.Name, pair.Second.Symbol).Observe(fiatAmount)
+	m.assetPurchaseAmountGauge.WithLabelValues(pair.First.Name, pair.First.Symbol, pair.Second.Name, pair.Second.Symbol).Set(amount)
+	m.assetPurchaseAmountHistogram.WithLabelValues(pair.First.Name, pair.First.Symbol, pair.Second.Name, pair.Second.Symbol).Observe(amount)
+	m.currencySpendAmountGauge.WithLabelValues(pair.Second.Name, pair.Second.Symbol, pair.Second.Name, pair.Second.Symbol).Set(fiatAmount)
+	m.currencySpendAmountHistogram.WithLabelValues(pair.Second.Name, pair.Second.Symbol, pair.Second.Name, pair.Second.Symbol).Observe(fiatAmount)
 	m.assetBalanceAmountGauge.WithLabelValues(pair.First.Name, pair.First.Symbol).Set(holdings)
 	m.assetBalanceValueGauge.WithLabelValues(pair.First.Name, pair.First.Symbol).Set(holdingsValue)
 }
